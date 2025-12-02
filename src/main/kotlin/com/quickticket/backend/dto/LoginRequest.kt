2bd9c357dc15a79ef675954974f28534e1841bde
Henry <figueroahenry828@gmail.com>
2025-12-02 00:49:52 -0300

    fix2

--- src/main/kotlin/com/quickticket/backend/dto/LoginRequest.kt
+++ src/main/kotlin/com/quickticket/backend/dto/LoginRequest.kt
@@ -2,6 +2,5 @@ package com.quickticket.backend.dto
 
 data class LoginRequest(
     val email: String,
-    val password: String,
-    val rut: String
+    val password: String
 )
--- src/main/kotlin/com/quickticket/backend/dto/RegisterRequest.kt
+++ src/main/kotlin/com/quickticket/backend/dto/RegisterRequest.kt
@@ -2,7 +2,7 @@ package com.quickticket.backend.dto
 
 data class RegisterRequest(
     val name: String,
-    val rut: String,
     val email: String,
-    val password: String
+    val password: String,
+    val rut: String
 )
